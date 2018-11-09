7b29ec88034d2e4933fc0434cf48cc5ab4bbc0a9
staxc <980301669@qq.com>
2018-11-09 14:46:26 +0800

    给参数添加注释

--- src/main/java/com/xgw/wwx/service/ftp/impl/FtpServiceImpl.java
+++ src/main/java/com/xgw/wwx/service/ftp/impl/FtpServiceImpl.java
@@ -26,11 +26,30 @@ public class FtpServiceImpl implements FtpService {
 	@Value("${spring.ftp.path}")
 	private String defaultPath;
 
+	/**
+	 *
+	 * @param path
+	 * 			FTP服务器保存目录
+	 * @param fileName
+	 *			上传到FTP服务器后的文件名称
+	 * @param is
+	 *			输入文件流
+	 * @return
+	 */
 	@Override
 	public boolean uploadFile(String path, String fileName, InputStream is) {
 		return FtpUtil.uploadFile(ftpHost, ftpPort, userName, passWord, path, fileName, is);
 	}
 
+	/**
+	 * @param remotePath
+	 * 				 FTP服务器文件目录
+	 * @param fileName
+	 * 				文件名称
+	 * @param localPath
+	 * 				下载后的文件路径
+	 * @return
+	 */
 	@Override
 	public boolean downLoadFile(String remotePath, String fileName, String localPath) {
 		return FtpUtil.downloadFile(ftpHost, ftpPort, userName, passWord, remotePath, fileName, localPath);
