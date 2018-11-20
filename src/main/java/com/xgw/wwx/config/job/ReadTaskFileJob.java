c2a8b6e345d2305dee34dc451ce6d7d993d35b72
staxc <980301669@qq.com>
2018-11-20 17:08:58 +0800

    测试成功

--- src/main/java/com/xgw/wwx/config/job/ReadTaskFileJob.java
+++ src/main/java/com/xgw/wwx/config/job/ReadTaskFileJob.java
@@ -28,14 +28,24 @@ public class ReadTaskFileJob implements Job {
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
         logger.debug("-- ReadTaskFileJob execute start --  "+ df.format(new Date()));
-       /* readAllFile("E:\\images");*/
-        getFile("E:\\images");
-        System.out.println(listname.size());
+        /*getFile("E:\\test");*/
+        List<File> files = getFiles("E:\\images");
+        for(File f : files){
+            System.out.println(f.getName());
+        }
+        System.out.println(files.size());
+
+
+       /* List<String> filess = getFile("E:\\images");
+        for(String fe : filess){
+            System.out.println(fe);
+        }
+        System.out.println(filess.size());*/
         logger.debug("-- ReadTaskFileJob execute end --  " + df.format(new Date()));
 
     }
     /**
-     * 获取文件夹下所有文件名
+     * 获取文件夹下所有文件名11
      */
     public static List<String> getFile(String path) {
         // get file list where the path has
@@ -52,25 +62,21 @@ public class ReadTaskFileJob implements Job {
         }
         return names;
     }
-
-   /* public void readAllFile(String filepath){
-        File file= new File(filepath);
+    /**
+     * 获取文件夹下所有文件名222
+     */
+    public static List<File> getFiles(String path){
+        File file = new File(path);
+        List<File> files = new ArrayList<File>();
         if(!file.isDirectory()){
-            listname.add(file.getName());
-        }else if(file.isDirectory()){
-            System.out.println("文件");
-            String[] filelist=file.list();
-            for(int i = 0;i<filelist.length;i++){
-                File readfile = new File(filepath);
-                if (!readfile.isDirectory()) {
-                    listname.add(readfile.getName());
-                } else if (readfile.isDirectory()) {
-                    readAllFile(filepath + "\\" + filelist[i]);//递归
-                }
+            files.add(file);
+        }else{
+            File[] subFiles = file.listFiles();
+            for(File f : subFiles){
+                files.addAll(getFiles(f.getAbsolutePath()));
             }
         }
-        for(int i = 0;i<listname.size();i++){
-            System.out.println(listname.get(i));
-        }
-    }*/
+        return files;
+    }
+
 }
