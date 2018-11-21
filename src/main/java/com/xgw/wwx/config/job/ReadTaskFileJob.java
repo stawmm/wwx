0154562749484544562033ee4014cfe3b3766ac4
staxc <980301669@qq.com>
2018-11-21 16:12:29 +0800

    定时读取任务

--- src/main/java/com/xgw/wwx/config/job/ReadTaskFileJob.java
+++ src/main/java/com/xgw/wwx/config/job/ReadTaskFileJob.java
@@ -1,5 +1,9 @@
 package com.xgw.wwx.config.job;
 
+import com.xgw.wwx.common.exception.WxxRuntimeException;
+import com.xgw.wwx.dto.db.CarryDTO;
+import com.xgw.wwx.dto.db.TaskFileDTO;
+import com.xgw.wwx.mapper.TaskFileMapper;
 import com.xgw.wwx.service.TaskService;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -10,16 +14,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class ReadTaskFileJob implements Job {
 
     private static final Logger logger = LoggerFactory.getLogger(ReadTaskFileJob.class);
 
-    private static ArrayList<String> listname = new ArrayList<String>();
-
+    private static final String TASK_FILE_PATH = "E:\\test";	//文件存放路径
 
     @Autowired
     private TaskService taskService;
@@ -28,21 +29,63 @@ public class ReadTaskFileJob implements Job {
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
         logger.debug("-- ReadTaskFileJob execute start --  "+ df.format(new Date()));
-        /*getFile("E:\\test");*/
-        List<File> files = getFiles("E:\\images");
-        for(File f : files){
-            System.out.println(f.getName());
+        List<CarryDTO> carryDTOS = null;
+        Long algId = 100000L;
+        try {
+            List<File> files = getFiles(TASK_FILE_PATH);
+            if (null == files || files.size() == 0) {
+                throw new WxxRuntimeException("FILE_CARRY_EMPTY", "破解文件为空");
+            }
+            // 遍历、破解文件
+            carryDTOS = new ArrayList<CarryDTO>();
+            Set<Long> hashModelSet = new HashSet<Long>();
+            for (File file : files) {
+                System.out.println(file.getName());
+                String carryPath = TASK_FILE_PATH + "/" + System.currentTimeMillis() + "_" + file.getName();
+                CarryDTO carryDTO = taskService.taskCarryOn(carryPath, algId);
+                logger.info("-- fileCarryOn message carryDTO=[{}] --", carryDTO);
+                if (!carryDTO.isResult()) {
+                    throw new WxxRuntimeException("FILE_CARRY_ERROR", "提串结果失败");
+                } else {
+                    Long hashMode = carryDTO.getHashModel();
+                    if (null == hashMode || !taskService.checkHashMode(hashMode)) {
+                        throw new WxxRuntimeException("ALG_ID_IS_EMPTY", "算法不支持: " + hashMode);
+                    }
+                }
+                carryDTO.setFileName(file.getName());
+                carryDTO.setCarryPath(carryPath);
+                // 天加到setl
+                hashModelSet.add(carryDTO.getHashModel());
+                // 添加到list
+                carryDTOS.add(carryDTO);
+            }// 算法类型不一致
+            if (hashModelSet.size() != 1) {
+                throw new WxxRuntimeException("FILE_ALG_ID_ERROR", "破解文件算法ID不一致");
+            }
+        }catch (WxxRuntimeException e) {
+            logger.error("-- fileCarryOn WxxRuntimeException error --", e);
+        } catch (Exception e) {
+            logger.error("-- fileCarryOn Exception error --", e);
         }
-        System.out.println(files.size());
-
-
-       /* List<String> filess = getFile("E:\\images");
-        for(String fe : filess){
-            System.out.println(fe);
-        }
-        System.out.println(filess.size());*/
         logger.debug("-- ReadTaskFileJob execute end --  " + df.format(new Date()));
 
+    }
+
+    /**
+     * 获取文件夹下所有文件名(可以递归拿到文件名)
+     */
+    public  List<File> getFiles(String path){
+        File file = new File(path);
+        List<File> files = new ArrayList<File>();
+        if(!file.isDirectory()){
+            files.add(file);
+        }else{
+            File[] subFiles = file.listFiles();
+            for(File f : subFiles){
+                files.addAll(getFiles(f.getAbsolutePath()));
+            }
+        }
+        return files;
     }
     /**
      * 获取文件夹下所有文件名(不能递归拿到文件)
@@ -65,7 +108,7 @@ public class ReadTaskFileJob implements Job {
     /**
      * 获取文件夹下所有文件名(可以递归拿到文件名)
      */
-    public static List<File> getFiles(String path){
+   /* public static List<File> getFiles(String path){
         File file = new File(path);
         List<File> files = new ArrayList<File>();
         if(!file.isDirectory()){
@@ -77,6 +120,6 @@ public class ReadTaskFileJob implements Job {
             }
         }
         return files;
-    }
+    }*/
 
 }
--- src/main/java/com/xgw/wwx/web/controller/TaskController.java
+++ src/main/java/com/xgw/wwx/web/controller/TaskController.java
@@ -10,6 +10,7 @@ import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.quartz.core.QuartzScheduler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
