6edc85322a92e600d4fad5e012fa0d1d4ab5035d
staxc <980301669@qq.com>
2018-11-29 09:54:09 +0800

    创建任务定时任务

--- src/main/java/com/xgw/wwx/config/job/ReadTaskFileJob.java
+++ src/main/java/com/xgw/wwx/config/job/ReadTaskFileJob.java
@@ -1,7 +1,9 @@
 package com.xgw.wwx.config.job;
 
 import com.xgw.wwx.common.exception.WxxRuntimeException;
-import com.xgw.wwx.dto.db.CarryDTO;
+import com.xgw.wwx.common.helper.AuthHelper;
+import com.xgw.wwx.dto.db.*;
+import com.xgw.wwx.service.HzLoggerService;
 import com.xgw.wwx.service.TaskService;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
@@ -11,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class ReadTaskFileJob implements Job {
@@ -23,49 +24,70 @@ public class ReadTaskFileJob implements Job {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    HzLoggerService hzLoggerService;
+
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-        logger.debug("-- ReadTaskFileJob execute start --  "+ df.format(new Date()));
+
+        logger.debug("-- ReadTaskFileJob execute start --  ");
+        //1.破解文件
         List<CarryDTO> carryDTOS = null;
         Long algId = 100000L;
-        try {
-            List<File> files = getFiles(TASK_FILE_PATH);
-            if (null == files || files.size() == 0) {
-                throw new WxxRuntimeException("FILE_CARRY_EMPTY", "破解文件为空");
-            }
-            // 遍历、破解文件
-            carryDTOS = new ArrayList<CarryDTO>();
-            Set<Long> hashModelSet = new HashSet<Long>();
-            for (File file : files) {
-                System.out.println(file.getName());
-                String carryPath = TASK_FILE_PATH + "/" + System.currentTimeMillis() + "_" + file.getName();
-                CarryDTO carryDTO = taskService.taskCarryOn(carryPath, algId);
-                logger.info("-- fileCarryOn message carryDTO=[{}] --", carryDTO);
-                if (!carryDTO.isResult()) {
-                    throw new WxxRuntimeException("FILE_CARRY_ERROR", "提串结果失败");
-                } else {
-                    Long hashMode = carryDTO.getHashModel();
-                    if (null == hashMode || !taskService.checkHashMode(hashMode)) {
-                        throw new WxxRuntimeException("ALG_ID_IS_EMPTY", "算法不支持: " + hashMode);
-                    }
-                }
-                carryDTO.setFileName(file.getName());
-                carryDTO.setCarryPath(carryPath);
-                // 天加到setl
-                hashModelSet.add(carryDTO.getHashModel());
-                // 添加到list
-                carryDTOS.add(carryDTO);
-            }// 算法类型不一致
-            if (hashModelSet.size() != 1) {
-                throw new WxxRuntimeException("FILE_ALG_ID_ERROR", "破解文件算法ID不一致");
-            }
-        }catch (WxxRuntimeException e) {
-            logger.error("-- fileCarryOn WxxRuntimeException error --", e);
-        } catch (Exception e) {
-            logger.error("-- fileCarryOn Exception error --", e);
+        List<File> files = getFiles(TASK_FILE_PATH);
+        if (null == files || files.size() == 0) {
+            throw new WxxRuntimeException("FILE_CARRY_EMPTY", "破解文件为空");
         }
-        logger.debug("-- ReadTaskFileJob execute end --  " + df.format(new Date()));
+        // 遍历、破解文件
+        carryDTOS = new ArrayList<CarryDTO>();
+        Set<Long> hashModelSet = new HashSet<Long>();
+        for (File file : files) {
+            System.out.println(file.getName());
+            String carryPath = TASK_FILE_PATH + "/" + System.currentTimeMillis() + "_" + file.getName();
+            CarryDTO carryDTO = taskService.taskCarryOn(carryPath, algId);
+            logger.info("-- fileCarryOn message carryDTO=[{}] --", carryDTO);
+            if (!carryDTO.isResult()) {
+                throw new WxxRuntimeException("FILE_CARRY_ERROR", "提串结果失败");
+            } else {
+                Long hashMode = carryDTO.getHashModel();
+                if (null == hashMode || !taskService.checkHashMode(hashMode)) {
+                    throw new WxxRuntimeException("ALG_ID_IS_EMPTY", "算法不支持: " + hashMode);
+                }
+            }
+            carryDTO.setFileName(file.getName());
+            carryDTO.setCarryPath(carryPath);
+            // 天加到setl
+            hashModelSet.add(carryDTO.getHashModel());
+            // 添加到list
+            carryDTOS.add(carryDTO);
+        }// 算法类型不一致
+        if (hashModelSet.size() != 1) {
+            throw new WxxRuntimeException("FILE_ALG_ID_ERROR", "破解文件算法ID不一致");
+        }
+
+        //2.配置资源
+        List<TaskResourceDTO> resourceList = taskService.findTaskResourceList();
+
+        //3.口令计算
+
+        //字典预估时间
+        TaskTimeCountDTO taskTimeCountDTO = new TaskTimeCountDTO();
+        Long count = taskService.vpndesTimeCount(taskTimeCountDTO);
+        //策略预估时间
+        logger.info("---------: {}", taskTimeCountDTO.getSpeed());
+        Long count1 = taskService.strategyTimeCount(taskTimeCountDTO);
+
+        //4.创建任务
+        TaskDTO taskDTO = new TaskDTO();
+        List<TaskFileDTO> filelist = taskDTO.getFiles();
+
+        // 批量创建任务
+        for (TaskFileDTO file1 : filelist) {
+            taskDTO.setFile(file1);
+            taskService.createTask(taskDTO);
+        }
+
+        logger.debug("-- ReadTaskFileJob execute end --  ");
 
     }
 
